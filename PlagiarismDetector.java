0ac844df360496c01ee03c2c11944e325057b9bf
gurpreetsingh <gurpreet24277@gmail.com>
2022-12-04 17:37:16 -0500

    Test files

--- PlagiarismDetector.java
+++ PlagiarismDetector.java
@@ -11,14 +11,20 @@
  */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class PlagiarismDetector {
     public static int checkPlagiarismWrapper(int[][] DP) {
         int plagiarismCounter = 0;
+        ArrayList<Integer> plagiarismCounterArr = new ArrayList<>();
         for (int i = 1; i < DP.length; i++) {
             for (int j = 1; j < DP[0].length; j++) {
-                if (DP[i - 1][j - 1] > 2 && (DP[i - 1][j - 1] > DP[i][j])) {
-                    plagiarismCounter += DP[i - 1][j - 1];
+                if(DP[i][j] > 2 && (i == DP.length - 1 || j == DP[0].length - 1)){
+                    plagiarismCounterArr.add(DP[i][j]);
+                    plagiarismCounter += plagiarismCounterArr.get(plagiarismCounterArr.size() - 1);
+                } else if(DP[i][j] > 2 && DP[i+1][j+1] == 0){
+                    plagiarismCounterArr.add(DP[i][j]);
+                    plagiarismCounter += plagiarismCounterArr.get(plagiarismCounterArr.size() - 1);
                 }
             }
         }
@@ -64,6 +70,7 @@ public class PlagiarismDetector {
                 strFile2.append(temp);
             }
 
+            // Normalization the String by preprocessing
             String inputStringFile1 = strFile1.toString()
                     .replaceAll("[^a-zA-Z0-9]", " ")
                     .replaceAll("\\s+", " ")
@@ -76,9 +83,14 @@ public class PlagiarismDetector {
                     .toLowerCase();
             String[] inputArrayFile2 = inputStringFile2.split(" ");
 
-            int resultPlagiarism = checkPlagiarism(inputArrayFile1, inputArrayFile2, inputArrayFile1.length, inputArrayFile2.length);
-            float finalResult = ((float) resultPlagiarism * 2 * 100) / (inputArrayFile1.length + inputArrayFile2.length);
+            int inputArrayFile1Len = inputArrayFile1.length;
+            int inputArrayFile2Len = inputArrayFile2.length;
+            int resultPlagiarism = checkPlagiarism(inputArrayFile1, inputArrayFile2, inputArrayFile1Len, inputArrayFile2Len);
 
+            // Taking Percentage which shows the similarity between two strings
+            float finalResult = ((float) resultPlagiarism * 2 * 100) / (inputArrayFile1Len + inputArrayFile2Len);
+            System.out.println(finalResult);
+            // Limit is set and Returns 0 for no similarity and 1 for similarity
             int plagiarism = (finalResult < 26) ? 0 : 1;
             return plagiarism;
 
@@ -87,6 +99,7 @@ public class PlagiarismDetector {
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
+            // Closing the open files
             readFile1.close();
             readFile2.close();
         }
@@ -99,19 +112,14 @@ public class PlagiarismDetector {
             if (args.length == 2) {
                 file1 = new File(args[0]);
                 file2 = new File(args[1]);
+                int response = filePreProcessing(file1, file2);
+                System.out.println(response);
             } else {
-                file1 = new File("/Users/gurpreetsingh/Concordia/Fall2022/Algorithms/Project/sample_data_and_submission/data/okay02/1.txt");
-                file2 = new File("/Users/gurpreetsingh/Concordia/Fall2022/Algorithms/Project/sample_data_and_submission/data/okay02/2.txt");
-
-                //file1 = new File("/Users/gurpreetsingh/Concordia/Fall2022/Algorithms/Project/sample_data_and_submission/data/plagiarism07/1.txt");
-                //file2 = new File("/Users/gurpreetsingh/Concordia/Fall2022/Algorithms/Project/sample_data_and_submission/data/plagiarism07/2.txt");
+                throw new Exception("File Not Found !!!");
             }
 
-            int response = filePreProcessing(file1, file2);
-            System.out.println(response);
-
         } catch (Exception e) {
-            System.out.println("ERR " + e.getMessage());
+            throw new Exception("ERR " + e.getMessage(), e);
         }
     }
 }
